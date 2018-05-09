824c35d086ded900d8f09f08fba5e2dd56db3724
crocomoth <justletmeplaymf@gmail.com>
2018-05-09 18:10:38 +0300

    Add project files.

---
+++ NET.S.2018.Popivnenko.20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2018.Popivnenko._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
